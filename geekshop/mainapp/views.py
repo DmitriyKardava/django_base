0c229ab0f03b6bed3ffb914e46aec2ba47ad0dea
Dmitriy Kardava <dmitriy.kardava@gmail.com>
2022-04-25 23:48:50 +0300

    load main menu from json

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,16 +1,29 @@
+import json
 from pathlib import Path
+from importlib.resources import contents
 from django.shortcuts import render
+from django.urls import reverse
+from geekshop.settings import BASE_DIR
+
+
+with open(BASE_DIR / 'mainapp' / 'data' / 'main_menu.json') as f:
+    main_menu = json.load(f)
+
 
 # Create your views here.
 
+# TODO: Создать view из main_menu.json  
 
 def index(request):
-    return render(request, Path('mainapp', 'index.html'))
+    return render(request, Path('mainapp', 'index.html'), 
+        context={'menu': main_menu})
 
 
 def products(request):
-    return render(request, Path('mainapp', 'products.html'))
+    return render(request, Path('mainapp', 'products.html'), 
+        context={'menu': main_menu})
 
 
 def contact(request):
-    return render(request, Path('mainapp', 'contact.html'))
+    return render(request, Path('mainapp', 'contact.html'), 
+        context={'menu': main_menu})
