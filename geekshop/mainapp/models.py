246595d055e7c64ea7762e196e58466be7321fae
Dmitriy Kardava <dmitriy.kardava@gmail.com>
2022-04-28 22:12:01 +0300

    lesson3

--- geekshop/mainapp/models.py
+++ geekshop/mainapp/models.py
@@ -1,3 +1,25 @@
+from unicodedata import category
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=32, unique=True)
+    description = models.CharField(max_length=128, blank=True)
+
+    def __str__(self) -> str:
+        return f'{self.id}: {self.name}'
+
+
+class Product(models.Model):
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=32, unique=True)
+    description = models.CharField(max_length=128, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    quantity = models.PositiveIntegerField(default=0)
+    image = models.ImageField(upload_to='products')
+
+    def __str__(self) -> str:
+        return f'{self.id}: {self.name}'
--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -2,7 +2,7 @@ import json
 from pathlib import Path
 from django.shortcuts import render
 from geekshop.settings import BASE_DIR
-
+from .models import Category, Product
 
 with open(BASE_DIR / 'mainapp' / 'data' / 'main_menu.json') as f:
     main_menu = json.load(f)
@@ -10,18 +10,25 @@ with open(BASE_DIR / 'mainapp' / 'data' / 'main_menu.json') as f:
 
 # Create your views here.
 
-# TODO: Создать view из main_menu.json  
+# TODO: Создать view из main_menu.json
 
 def index(request):
-    return render(request, Path('mainapp', 'index.html'), 
-        context={'menu': main_menu})
+    return render(request, Path('mainapp', 'index.html'),
+                  context={'menu': main_menu})
 
 
 def products(request):
-    return render(request, Path('mainapp', 'products.html'), 
-        context={'menu': main_menu})
+    categories = Category.objects.all()
+    products = Product.objects.all()
+
+    return render(request, Path('mainapp', 'products.html'),
+                  context={
+                      'title': 'Продукты',
+                      'menu': main_menu,
+                      'categories': categories,
+                      'products': products})
 
 
 def contact(request):
-    return render(request, Path('mainapp', 'contact.html'), 
-        context={'menu': main_menu})
+    return render(request, Path('mainapp', 'contact.html'),
+                  context={'menu': main_menu})
