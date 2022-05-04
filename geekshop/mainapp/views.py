d014673246fbe35442bde541364b6aabcad3d47c
Dmitriy Kardava <dmitriy.kardava@gmail.com>
2022-05-04 12:04:37 +0300

    corrected json encoding

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -4,7 +4,8 @@ from django.shortcuts import render
 from geekshop.settings import BASE_DIR
 from .models import Category, Product
 
-with open(BASE_DIR / 'mainapp' / 'data' / 'main_menu.json') as f:
+with open(BASE_DIR / 'mainapp' / 'data' / 'main_menu.json',  
+            encoding='utf-8') as f:
     main_menu = json.load(f)
 
 
