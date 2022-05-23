ea521e61248e43735e4b8e34121caa1638155fdf
Dmitriy Kardava <dmitriy.kardava@gmail.com>
2022-05-23 22:08:11 +0300

    lesson_7

--- geekshop/authapp/models.py
+++ geekshop/authapp/models.py
@@ -8,3 +8,4 @@ class ShopUser(AbstractUser):
     avatar = models.ImageField(upload_to='users_avatars', blank=True)
     age = models.PositiveIntegerField(
         verbose_name='возраст', blank=True, null=True)
+    is_active = models.BooleanField(verbose_name='активный', default=True)
--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -21,10 +21,11 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('auth/', include('authapp.urls', namespace='auth')),
     path('cart/', include('cartapp.urls', namespace='cart')),
     path('products/<int:pk>', mainapp.category, name='category'),
+    path('admin/', include('adminapp.urls', namespace='admin'))
 ]
 
 
