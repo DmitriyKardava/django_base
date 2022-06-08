b8ea8cd20b5045927de2f12d151e7975b6839391
Dmitriy Kardava <dmitriy.kardava@gmail.com>
2022-06-08 21:01:51 +0300

    django2_lesson1

--- geekshop/authapp/models.py
+++ geekshop/authapp/models.py
@@ -11,9 +11,9 @@ class ShopUser(AbstractUser):
     avatar = models.ImageField(upload_to='users_avatars', blank=True)
     age = models.PositiveIntegerField(
         verbose_name='возраст', blank=True, null=True)
-    activatio_key = models.UUIDField(default=uuid.uuid4)
+    activation_key = models.UUIDField(default=uuid.uuid4)
     activation_key_expires = models.DateTimeField(default=(now() + timedelta(hours=48)))
     is_active = models.BooleanField(verbose_name='активный', default=False)
     
-    def is_activatio_key_expired(self):
+    def is_activation_key_expired(self):
         return now() > self.activation_key_expires
