589c7e19a6b257be4e53a1d2750a1dee3a714d17
Dmitriy Kardava <dmitriy.kardava@gmail.com>
2022-06-12 10:37:07 +0300

    django2_lesson3

--- geekshop/ordersapp/urls.py
+++ geekshop/ordersapp/urls.py
@@ -6,6 +6,7 @@ app_name = 'orders_app'
 urlpatterns = [
     path("", ordersapp.OrderList.as_view(), name="list"),
     path("create", ordersapp.create_order, name="create"),
-    path("pay/<int:pk>", ordersapp.order_pay, name="pay"),
-    path("cancel/<int:pk>", ordersapp.order_cancel, name="cancel"),
+    path("<int:pk>/", ordersapp.OrderUpdateView.as_view(), name="update"),
+    path("pay/<int:pk>/", ordersapp.order_pay, name="pay"),
+    path("cancel/<int:pk>/", ordersapp.order_cancel, name="cancel"),
 ]
